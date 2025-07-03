98f8b67be5272cb47c549e0a355e62ece2b1874f
Narayan Behera <146068913+lijubehera@users.noreply.github.com>
2025-07-03 10:04:57 +0530

    Add files via upload

---
+++ day9.js
@@ -0,0 +1,9 @@
+let a = 6;
+let b = 5;
+
+let cond1 = a > b;       // true
+let cond2 = a === 5;     // false
+
+console.log("cond1 && cond2 =", cond1 && cond2); // false
+console.log("cond1 || cond2 =", cond1 || cond2); // true
+console.log("!cond1 =", !cond1);                 // false
